21dcb22bd2b6fe98af1998034cb561339665ff53
League <league@WorthyMini.local>
2019-11-30 11:19:48 -0800

    wassup almost done
    don't delete op

--- src/_06_for_loop_gauntlet/ForLoopGauntlet.java
+++ src/_06_for_loop_gauntlet/ForLoopGauntlet.java
@@ -2,24 +2,42 @@ package _06_for_loop_gauntlet;
 
 public class ForLoopGauntlet {
     public static void main(String[] args) {
-//        for (int x=0;x<=100;x++) {
-//            System.out.println(x);
-//
-//        }
-//      for (int y=100; y>=0; y--) {
-//      System.out.println(y);
-//      }
-//        for (int z = 2; z <= 100; z++) {
-//            if (z % 2==0) {
-//                System.out.println(z);
-//            }
-//        }
-        for (int w = 1; w <= 99; w++) {
-        if (w%2==1) {
-        System.out.println(w);
-        }
-        }
-        }
+// for (int x=0;x<=100;x++) {
+// System.out.println(x);
+// }
+// for (int y=100; y>=0; y--) {
+//  System.out.println(y);
+// }
+// for (int z = 2; z <= 100; z++) {
+// if (z % 2==0) {
+// System.out.println(z);
+// }
+// }
+// for (int w = 1; w <= 99; w++) {
+// if (w % 2 == 1) {
+// System.out.println(w);
+// for (int w = 1; w <= 500; w++) {
+// if (w % 2 == 1) {
+// System.out.print(w);
+// System.out.println(" odd");
+// } else {
+// System.out.print(w);
+// System.out.println(" even");
+// for (int x = 7; x <= 777; x++) {
+// if (x % 7 == 0) {
+// System.out.println(x);
+// }
+// }
+
     }
+}
+
+
+
+
+
+
+
+
 
 
